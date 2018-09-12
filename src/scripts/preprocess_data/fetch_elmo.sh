318584c0c1caed6c22d953bc8af4818c2dd94d0c
Jakub Chledowski <jakub.chledowski@gmail.com>
2018-09-12 11:05:01 +0200

    ez dataset

--- src/scripts/preprocess_data/fetch_elmo.sh
+++ src/scripts/preprocess_data/fetch_elmo.sh
@@ -11,5 +11,5 @@ mkdir -p $DATA_DIR/elmo
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json
 mv elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5 ${DATA_DIR}/elmo/weights.h5
-mv elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json${DATA_DIR}/elmo/options.json
+mv elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json ${DATA_DIR}/elmo/options.json
 
