d5103b8943882d5c074ee507662a0ed99e71c4d3
tomwesolowski <tom.wesolowski92@gmail.com>
2018-10-25 18:31:53 +0200

    remove src directory

--- src/models/kim/data/fetch_and_preprocess.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-python download.py
-python preprocess_data.py
--- src/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-# level of general logging
-SCREEN_LOG_LEVEL = logging.INFO
-FILE_LOG_LEVEL = logging.DEBUG
